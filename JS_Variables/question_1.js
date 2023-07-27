c009ae0487816f9bdf6e2cb4018ffe2e278362cc
Rohit Nale <rohitnale738@gmail.com>
2023-07-27 18:49:50 +0530

    Assignment 2

---
+++ JS_Variables/question_1.js
@@ -0,0 +1,22 @@
+// Declare a variable
+
+let message;
+console.log(message);
+
+message = 'Hello World !!';
+
+console.log(message);
+
+// Declare a constant variable
+
+const birthday;
+console.log(birthday);
+
+birthday = '08.04.1997';
+
+console.log(birhtday);
+
+
+// Getting Below Issue:
+// Uncaught SyntaxError: Missing initializer in const declaration
+// When we are declaring the constant variable at the same time assign the value them.
